25a64fcfa43b515abf07777886e6145972cf1f12
Jianmin Chen <jianmin@jianminchen.com>
2023-07-18 11:29:20 -0400

    Update with new functions

--- main.cpp
+++ main.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
 
+// function declaration
+int add(int x, int y);
+int subtract(int x, int y);
+int multiply(int x, int y);
+int divide(int x, int y);
+
+// function definition
+int add(int x, int y)
+{
+  return x + y;
+}
+
+int subtract(int x, int y) {
+  return x - y;
+}
+
+int multiply(int x, int y) {
+  return x * y;
+}
+
+int divide(int x, int y) {
+  return x / y;
+}
+
 int main()
 {
-  std::cout << "Hello, world!" << std::endl;
-
-  int x = 5;
-  int y = 7;
-  std::cout << "The sum of " << x << " and " << y << " is " << x + y << std::endl;
+  int x = 10;
+  int y = 5;
+  int sum = add(x, y); // call the add function
+  int difference = subtract(x, y);
+  int product = multiply(x, y);
+  int quotient = divide(x, y);
+  std::cout << "The sum of " << x << " and " << y << " is " << sum << std::endl;
+  std::cout << "The difference of " << x << " and " << y << " is " << difference << std::endl;
+  std::cout << "The product of " << x << " and " << y << " is " << product << std::endl;
+  std::cout << "The quotient of " << x << " and " << y << " is " << quotient << std::endl;
 
   return 0;
 }
